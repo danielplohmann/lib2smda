a1997f628b087c80054be04a071231b803d7903c
pnx@pyrite <daniel.plohmann@fkie.fraunhofer.de>
2023-07-31 09:43:18 +0200

    fixed typo

--- export_mcrit.py
+++ export_mcrit.py
@@ -7,6 +7,6 @@ c = McritClient()
 sample_id_to_entry = c.getSamples()
 for sample_id, sample_entry in sample_id_to_entry.items():
     exported_json = c.getExportData([sample_id])
-    bitness = "_x64" if sample_entry.bitness == 64 else "_86"
+    bitness = "_x64" if sample_entry.bitness == 64 else "_x86"
     with open(sample_entry.filename[:-3]+ bitness + ".mcrit", "w") as fout:
         json.dump(exported_json, fout, indent=1)
